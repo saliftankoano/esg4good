09aece42ff09cf81bef5a70874af2aa30b94d153
KaBankz <20971850+KaBankz@users.noreply.github.com>
2025-01-19 11:47:16 -0500

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,39 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @see https://github.com/tailwindlabs/prettier-plugin-tailwindcss
+ * @see https://github.com/ianvs/prettier-plugin-sort-imports
+ *
+ * @typedef {import("prettier").Config} PrettierConfig
+ * @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig
+ * @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig
+ *
+ * @type { PrettierConfig | SortImportsConfig | TailwindConfig }
+ */
+const config = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSameLine: true,
+  htmlWhitespaceSensitivity: "ignore",
+  trailingComma: "es5",
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  tailwindFunctions: ["tv", "cn"],
+  importOrderTypeScriptVersion: "5.0.0",
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "",
+    "^(react/(.*)$)|^(react$)",
+    "^(next(.*)$)|^(next$)",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/(.*)$",
+    "",
+    "^[./]",
+    "^[../]",
+  ],
+};
+
+export default config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,28 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      animation: {
+        gradient: "gradient 6s linear infinite",
+        "pulse-slow": "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+      keyframes: {
+        gradient: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+      backgroundSize: {
+        200: "200% 200%",
+      },
+      transitionDelay: {
+        700: "700ms",
+        1000: "1000ms",
       },
     },
   },
