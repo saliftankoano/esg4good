22c44690756da0c8aff6a03f45dc55b81e514379
KaBankz <20971850+KaBankz@users.noreply.github.com>
2025-01-20 23:38:39 -0500

    Add initial map

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -1,3 +1,29 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
+import mapboxgl from 'mapbox-gl';
+
 export default function MapsPage() {
-  return <div>Maps</div>;
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<mapboxgl.Map>(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
+    if (mapContainerRef.current) {
+      mapRef.current = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: 'mapbox://styles/mapbox/dark-v11',
+        center: [-74.0242, 40.6941],
+        zoom: 10.12,
+      });
+    }
+
+    return () => {
+      mapRef.current?.remove();
+    };
+  }, []);
+
+  return <main ref={mapContainerRef} className='h-[100vh] w-[100vw]' />;
 }
