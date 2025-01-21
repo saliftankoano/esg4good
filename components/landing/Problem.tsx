265efa4d451ec73de966b40419151e7dbcd1f147
Salif Tankoano <tanksalif@gmail.com>
2025-01-21 13:34:01 -0500

    orbit draft

--- components/landing/Problem.tsx
+++ components/landing/Problem.tsx
@@ -1,7 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import {
+  AlertTriangleIcon,
+  BanknoteIcon,
+  ClockIcon,
+  FactoryIcon,
+  LandmarkIcon,
+  PackageIcon,
+  PowerIcon,
+  ScaleIcon,
+  TrendingUpIcon,
+} from 'lucide-react';
+
+import { OrbitingCircles } from '../ui/orbiting-circles';
+
 export default function Problem() {
   return (
     <section id='problem' className='bg-neutral-50 py-24 text-black'>
-      <div className='container mx-auto px-4'>
+      <div className='container mx-auto flex flex-col items-center justify-center px-4'>
         <div className='mx-auto mb-16 max-w-3xl text-center'>
           <h2 className='mb-6 text-4xl font-bold'>
             The Challenge in ESG Reporting
@@ -11,28 +26,51 @@ export default function Problem() {
             collection, and measuring real impact.
           </p>
         </div>
-        <div className='grid grid-cols-3 gap-8'>
-          <div className='rounded-xl bg-white p-8 shadow-sm'>
-            <i className='fa-solid fa-clipboard-list mb-4 text-3xl'></i>
-            <h3 className='mb-3 text-xl font-semibold'>Complex Compliance</h3>
-            <p className='text-neutral-600'>
-              Navigate through numerous regulations and frameworks.
-            </p>
-          </div>
-          <div className='rounded-xl bg-white p-8 shadow-sm'>
-            <i className='fa-solid fa-database mb-4 text-3xl'></i>
-            <h3 className='mb-3 text-xl font-semibold'>Data Management</h3>
-            <p className='text-neutral-600'>
-              Scattered data sources and inconsistent metrics.
-            </p>
-          </div>
-          <div className='rounded-xl bg-white p-8 shadow-sm'>
-            <i className='fa-solid fa-chart-column mb-4 text-3xl'></i>
-            <h3 className='mb-3 text-xl font-semibold'>Impact Tracking</h3>
-            <p className='text-neutral-600'>
-              Difficulty in measuring and proving sustainability impact.
-            </p>
-          </div>
+        <div className='relative flex h-[500px] w-1/2 flex-col items-center justify-center overflow-hidden rounded-lg'>
+          <span className='pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-6xl font-semibold leading-none text-transparent dark:from-white dark:to-black'>
+            ESG
+          </span>
+
+          <OrbitingCircles iconSize={40}>
+            <div className='flex items-center gap-2'>
+              <BanknoteIcon className='h-4 w-4' />
+              <span className='text-sm font-medium'>High Capital Costs</span>
+            </div>
+            <div className='flex items-center gap-2'>
+              <ScaleIcon className='h-4 w-4' />
+              <span className='text-sm font-medium'>Regulatory Hurdles</span>
+            </div>
+            <div className='flex items-center gap-2'>
+              <PowerIcon className='h-4 w-4' />
+              <span className='text-sm font-medium'>Grid Integration</span>
+            </div>
+            <div className='flex items-center gap-2'>
+              <TrendingUpIcon className='h-4 w-4' />
+              <span className='text-sm font-medium'>Market Volatility</span>
+            </div>
+            <div className='flex items-center gap-2'>
+              <AlertTriangleIcon className='h-4 w-4' />
+              <span className='text-sm font-medium'>Tech Risks</span>
+            </div>
+          </OrbitingCircles>
+          <OrbitingCircles iconSize={30} radius={100} reverse speed={2}>
+            <div className='flex items-center gap-2'>
+              <LandmarkIcon className='h-4 w-4' />
+              <span className='text-sm font-medium'>Policy Changes</span>
+            </div>
+            <div className='flex items-center gap-2'>
+              <ClockIcon className='h-4 w-4' />
+              <span className='text-sm font-medium'>Long ROI</span>
+            </div>
+            <div className='flex items-center gap-2'>
+              <PackageIcon className='h-4 w-4' />
+              <span className='text-sm font-medium'>Supply Chain</span>
+            </div>
+            <div className='flex items-center gap-2'>
+              <FactoryIcon className='h-4 w-4' />
+              <span className='text-sm font-medium'>Infrastructure</span>
+            </div>
+          </OrbitingCircles>
         </div>
       </div>
     </section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,94 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
+    darkMode: ['class'],
+    content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      animation: {
-        gradient: 'gradient 6s linear infinite',
-        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-      },
-      keyframes: {
-        gradient: {
-          '0%, 100%': {
-            'background-size': '200% 200%',
-            'background-position': 'left center',
-          },
-          '50%': {
-            'background-size': '200% 200%',
-            'background-position': 'right center',
-          },
-        },
-      },
-      backgroundSize: {
-        200: '200% 200%',
-      },
-      transitionDelay: {
-        700: '700ms',
-        1000: '1000ms',
-      },
-    },
+  	extend: {
+  		animation: {
+  			gradient: 'gradient 6s linear infinite',
+  			'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+  			orbit: 'orbit calc(var(--duration)*1s) linear infinite'
+  		},
+  		keyframes: {
+  			gradient: {
+  				'0%, 100%': {
+  					'background-size': '200% 200%',
+  					'background-position': 'left center'
+  				},
+  				'50%': {
+  					'background-size': '200% 200%',
+  					'background-position': 'right center'
+  				}
+  			},
+  			orbit: {
+  				'0%': {
+  					transform: 'rotate(calc(var(--angle) * 1deg)) translateY(calc(var(--radius) * 1px)) rotate(calc(var(--angle) * -1deg))'
+  				},
+  				'100%': {
+  					transform: 'rotate(calc(var(--angle) * 1deg + 360deg)) translateY(calc(var(--radius) * 1px)) rotate(calc((var(--angle) * -1deg) - 360deg))'
+  				}
+  			}
+  		},
+  		backgroundSize: {
+  			'200': '200% 200%'
+  		},
+  		transitionDelay: {
+  			'700': '700ms',
+  			'1000': '1000ms'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
