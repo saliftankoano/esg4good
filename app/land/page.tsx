4c22559f27b9bef29c3164cb3f07e7a2b716af62
Salif Tankoano <tanksalif@gmail.com>
2025-01-21 12:21:29 -0500

    move solutions

--- app/land/page.tsx
+++ app/land/page.tsx
@@ -3,8 +3,8 @@ import Footer from '@/components/landing/Footer';
 import Header from '@/components/landing/Header';
 import Hero from '@/components/landing/Hero';
 import Problem from '@/components/landing/Problem';
+import Solution from '@/components/landing/Solution';
 import Testimonials from '@/components/landing/Testimonials';
-import Solution from '@/components/Solution';
 
 export default function Land() {
   return (
