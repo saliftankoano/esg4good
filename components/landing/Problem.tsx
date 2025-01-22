068225dd3d280b7ef4af39ce7c0ab09d4a245bdf
Salif Tankoano <tanksalif@gmail.com>
2025-01-22 11:46:36 -0500

    update orbits

--- components/landing/Problem.tsx
+++ components/landing/Problem.tsx
@@ -1,14 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {
   AlertTriangleIcon,
-  BanknoteIcon,
-  ClockIcon,
+  CircleDollarSign,
   FactoryIcon,
-  LandmarkIcon,
-  PackageIcon,
-  PowerIcon,
-  ScaleIcon,
+  GemIcon,
+  LeafIcon,
+  MapPin,
+  SearchIcon,
   TrendingUpIcon,
+  UsersIcon,
 } from 'lucide-react';
 
 import { OrbitingCircles } from '../ui/orbiting-circles';
@@ -19,56 +18,68 @@ export default function Problem() {
       <div className='container mx-auto flex flex-col items-center justify-center px-4'>
         <div className='mx-auto mb-16 max-w-3xl text-center'>
           <h2 className='mb-6 text-4xl font-bold'>
-            The Challenge in ESG Reporting
+            Challenges at Inception & Funding
           </h2>
           <p className='text-lg text-neutral-600'>
-            Companies struggle with complex reporting requirements, data
-            collection, and measuring real impact.
+            Understanding the landscape of renewable energy, it&apos;s risks,
+            and its impact is complex.
           </p>
         </div>
         <div className='relative flex h-[500px] w-1/2 flex-col items-center justify-center overflow-hidden rounded-lg'>
-          <span className='pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-6xl font-semibold leading-none text-transparent dark:from-white dark:to-black'>
+          <span className='pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-green-800 to-green-500 bg-clip-text text-center text-6xl font-semibold leading-none text-transparent dark:from-white dark:to-black'>
             ESG
           </span>
 
-          <OrbitingCircles iconSize={40}>
+          <OrbitingCircles iconSize={40} radius={220}>
             <div className='flex items-center gap-2'>
-              <BanknoteIcon className='h-4 w-4' />
-              <span className='text-sm font-medium'>High Capital Costs</span>
+              <SearchIcon className='h-4 w-4 text-red-500' />
+              <span className='text-sm font-bold text-red-500'>
+                Due Diligence
+              </span>
             </div>
             <div className='flex items-center gap-2'>
-              <ScaleIcon className='h-4 w-4' />
-              <span className='text-sm font-medium'>Regulatory Hurdles</span>
+              <GemIcon className='h-4 w-4 text-blue-500' />
+              <span className='text-sm font-bold text-blue-500'>
+                Spot Opportunities
+              </span>
             </div>
             <div className='flex items-center gap-2'>
-              <PowerIcon className='h-4 w-4' />
-              <span className='text-sm font-medium'>Grid Integration</span>
+              <MapPin className='h-4 w-4 text-red-500' />
+              <span className='text-sm font-bold text-red-500'>Location</span>
             </div>
             <div className='flex items-center gap-2'>
-              <TrendingUpIcon className='h-4 w-4' />
-              <span className='text-sm font-medium'>Market Volatility</span>
+              <TrendingUpIcon className='h-4 w-4 text-red-500' />
+              <span className='text-sm font-bold text-red-500'>
+                Competitors
+              </span>
             </div>
             <div className='flex items-center gap-2'>
-              <AlertTriangleIcon className='h-4 w-4' />
-              <span className='text-sm font-medium'>Tech Risks</span>
+              <AlertTriangleIcon className='h-4 w-4 text-red-500' />
+              <span className='text-sm font-bold text-red-500'>Risks</span>
             </div>
           </OrbitingCircles>
-          <OrbitingCircles iconSize={30} radius={100} reverse speed={2}>
+          <OrbitingCircles iconSize={30} radius={120} reverse speed={2}>
             <div className='flex items-center gap-2'>
-              <LandmarkIcon className='h-4 w-4' />
-              <span className='text-sm font-medium'>Policy Changes</span>
+              <LeafIcon className='h-4 w-4 text-green-700' />
+              <span className='text-sm font-bold text-green-700'>
+                Environmental Impact
+              </span>
             </div>
             <div className='flex items-center gap-2'>
-              <ClockIcon className='h-4 w-4' />
-              <span className='text-sm font-medium'>Long ROI</span>
+              <UsersIcon className='h-4 w-4 text-green-700' />
+              <span className='text-sm font-bold text-green-700'>
+                Social Impact
+              </span>
             </div>
             <div className='flex items-center gap-2'>
-              <PackageIcon className='h-4 w-4' />
-              <span className='text-sm font-medium'>Supply Chain</span>
+              <CircleDollarSign className='h-4 w-4 text-green-700' />
+              <span className='text-sm font-bold text-green-700'>ROI</span>
             </div>
             <div className='flex items-center gap-2'>
-              <FactoryIcon className='h-4 w-4' />
-              <span className='text-sm font-medium'>Infrastructure</span>
+              <FactoryIcon className='h-4 w-4 text-green-700' />
+              <span className='text-sm font-bold text-green-700'>
+                Infrastructure
+              </span>
             </div>
           </OrbitingCircles>
         </div>
